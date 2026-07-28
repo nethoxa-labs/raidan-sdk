b195f7a808993b364aff720e73b05449c0764d5e
nethoxa <135072738+nethoxa@users.noreply.github.com>
2026-07-28 20:04:36 +0200

    chore(release): bump SDK version to v0.1.8

--- sdk.go
+++ sdk.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Version identifies this SDK release.
-const Version = "v0.1.7"
+const Version = "v0.1.8"
 
 // Target describes the execution and consensus endpoints exercised by a case.
 type Target = target.Target
